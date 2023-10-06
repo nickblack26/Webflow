a7bcf5904ea0294c8f327f8cea8af194acfeb375
Nick Black <49686365+nickblack26@users.noreply.github.com>
2023-10-06 17:33:50 -0500

    Initial Commit

---
+++ Webflow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Webflow
+//
+//  Created by Nick on 10/6/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
